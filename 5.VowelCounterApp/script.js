6cf758e2325c9315d5e05820b017ccf03daa5301
LayaneB <layanejuvito@gmail.com>
2022-01-10 17:23:50 -0300

    adding other way to do the counting

--- 5.VowelCounterApp/script.js
+++ 5.VowelCounterApp/script.js
@@ -7,20 +7,30 @@ btn.addEventListener('click', countVowel);
 function countVowel(){
     const input=document.querySelector('.inputWord');
     const inValue = input.value.toLowerCase();
-    let numVoewl = 0;
+    let numVowel = 0;
     console.log(inValue);
     let len= inValue.length;
     for(let i=0;i<len;i++){
         if(inValue[i]=='a'|| inValue[i]=='e'|| inValue[i]=='i' || inValue[i]=='o' || inValue[i]=='u'){
-            numVoewl++;
+            numVowel++;
         }
         if(inValue[i]=='á'|| inValue[i]=='é'|| inValue[i]=='í' || inValue[i]=='ó' || inValue[i]=='ú'){
-            numVoewl++;
+            numVowel++;
         }
         if(inValue[i]=='â'|| inValue[i]=='ê'|| inValue[i]=='ã' || inValue[i]=='õ' || inValue[i]=='ô'){
-            numVoewl++;
+            numVowel++;
         }
     }
 
-    return result.innerHTML = `The number of vowels in the word ${inValue} is ${numVoewl}`;
+    return result.innerHTML = `The number of vowels in the word ${inValue} is ${numVowel}`;
 }
+
+
+// ohter way to do this:
+// for(let i=0;i<len;i++){
+//     let letter=inValue[i]; 
+//     if(letter.match(/([a,e,i,o,u]))){
+//         numVowel++
+//     }
+// }
+// return result.innerHTML = `The number of vowels in the word ${inValue} is ${numVowel}`;
